800fad413111109e29404f1c0358b881d43f2c60
Your Name <you@example.com>
2024-08-14 10:09:26 +0530

    the problem file

---
+++ DSA/Arrays.java
@@ -0,0 +1,11 @@
+ package  DSA;
+
+
+public class Arrays {
+    public static void main(String[] args) {
+        int []arr = {10,20,30,40,50};
+        for(int i=0; i< arr.length; i++ ){
+            System.err.println(arr[i]);
+        } 
+    }
+}
---
+++ staircase.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public  class staircase{
+    public  static void main(String []args){
+        Scanner n = new Scanner(System.in);
+        int a = n.nextInt();
+        System.out.print(a);
+    }
+}
